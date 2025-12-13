d6ac96d31521a6e146d6e66b907cd2e31d7c1cfd
Ezequiel Quiroz <quirozarielezequiel@gmail.com>
2025-12-12 22:07:47 -0300

    fix(marcas): corregir error de tipeo en MarcasPage.jsx

--- src/features/brands/pages/MarcasPage.jsx
+++ src/features/brands/pages/MarcasPage.jsx
@@ -22,7 +22,7 @@ export default function MarcasPage() {
       ) : (
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
           {marcas.map((marca) => (
-            <BrandCard
+            <BrandsCard
               key={marca.id}
               marca={marca}
             />
