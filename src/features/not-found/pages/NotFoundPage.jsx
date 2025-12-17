ddf3f4d50afd1b8615ced67a0efc4b908a7d66b1
anitaprg <anapaulariosgarin@gmail.com>
2025-12-17 02:37:11 -0300

    feat(not-found): adición de funcionalidad de error 404.

---
+++ src/features/not-found/pages/NotFoundPage.jsx
@@ -0,0 +1,9 @@
+import NotFoundMessage from "../components/NotFoundMessage";
+
+export default function NotFoundPage() {
+  return (
+    <section className="min-h-[70vh] flex items-center justify-center px-4">
+      <NotFoundMessage />
+    </section>
+  );
+}
